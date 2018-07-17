caa5fc4fc7a606ef72ce4add5104657d74762e9d
hjz <841641127@qq.com>
2018-07-17 15:21:12 +0800

    第二部分完成

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from extra_apps import xadmin
 from django.views.generic import TemplateView
+from users.views import LoginView
+
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'),name='index'),
-    path('login/', TemplateView.as_view(template_name='login.html'),name='login'),
+    path('login/', LoginView.as_view(), name = 'login'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,27 +2,38 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.shortcuts import render
+from django.views.generic.base import View
 
+from .form import LoginForm
 from .models import UserProfile
 
+from .form import LoginForm
 
-def user_login(request):
-    if request.method == 'POST':
-        # 获取用户提交的用户名和密码
-        user_name = request.POST.get('username',None)
-        pass_word = request.POST.get('password',None)
-        # 成功返回user对象,失败None
-        user = authenticate(username=user_name,password=pass_word)
-        # 如果不是null说明验证成功
-        if user is not None:
-            # 登录
-            login(request,user)
-            return render(request,'index.html')
+class LoginView(View):
+    def get(self,request):
+        return render(request, 'login.html')
+
+    def post(self,request):
+        # 实例化
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            # 获取用户提交的用户名和密码
+            user_name = request.POST.get('username', None)
+            pass_word = request.POST.get('password', None)
+            # 成功返回user对象,失败None
+            user = authenticate(username=user_name, password=pass_word)
+            # 如果不是null说明验证成功
+            if user is not None:
+                # 登录
+                login(request, user)
+                return render(request, 'index.html')
+            # 只有当用户名或密码不存在时，才返回错误信息到前端
+            else:
+                return render(request, 'login.html', {'msg': '用户名或密码错误','login_form':login_form})
+            
+        # form.is_valid（）已经判断不合法了，所以这里不需要再返回错误信息到前端了
         else:
-            return render(request,'login.html',{'msg':'用户名或密码错误'})
-    
-    elif request.method == 'GET':
-        return render(request,'login.html')
+            return render(request,'login.html',{'login_form':login_form})
 
 
 #邮箱和用户名都可以登录
